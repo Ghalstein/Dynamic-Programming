9948c3c0051fd775b3256fd5eca8a9b3af8e76f1
Garrett Halstein <garrett.halstein@emory.edu>
2019-11-01 23:28:59 -0400

    made the function for the LCS

--- LCS.java
+++ LCS.java
@@ -1,6 +1,37 @@
+/*
+* Finds the longest common subsequence
+* Have to break the problem down into subproblems
+* The subproblem is compare one index to the beginning of the first index
+* Keep track of the longest sequence
+*/
+
+
+
 public class LCS {
 
+  // finds the longest subsequence
+  public static int find(String s1, String s2) {
+
+    int longest = 0;
+
+    // iterate through s1 and find whether s2 has equal char for each index
+    int i = 0;
+    while (i < s1.length()) {
+      int curr2 = 0;
+      int curr1 = i;
+      int relLongest = 0;
+      while (curr2 < s2.length()) {
+        if (s1.charAt(curr1) == s2.charAt(curr2)) {
+          ++relLongest;
+          if (relLongest > longest)
+            longest = relLongest;
+        }
+      }
+      ++i;
+    }
+  }
+
   public static void main(String[] args) {
-    
+
   }
 }
